064ea7db1c78a5569bd27678cf78cb30426f80ca
henrylong <henrylong719@gmail.com>
2021-08-24 22:14:00 +0930

    add more queries and mutation

--- job-board/client/src/requests.js
+++ job-board/client/src/requests.js
@@ -1,24 +1,93 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
-export async function loadJobs() {
+async function graphqlRequest(query, variables = {}) {
   const response = await fetch(endpointURL, {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({
-      query: `
-      {
-        jobs {
-          id
-          title
-          company{ 
-            id
-            name
-          }
-        }
-      }`,
+      query,
+      variables,
     }),
   });
 
   const responseBody = await response.json();
-  return responseBody.data.jobs;
+
+  if (responseBody.errors) {
+    const message = responseBody.errors
+      .map((error) => error.message)
+      .join('\n');
+
+    throw new Error(message);
+  }
+
+  return responseBody.data;
+}
+
+export async function createJob(input) {
+  const mutation = `
+  mutation CreateJob($input: CreateJobInput){
+    job: createJob(input: $input){
+      id
+      title
+      company{
+        id
+        name
+      }
+    }
+  }`;
+
+  const { job } = await graphqlRequest(mutation, { input });
+  return job;
+}
+
+export async function loadJob(id) {
+  const query = `query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      company{
+        id
+        name
+      }
+      description
+    }
+  }`;
+
+  const { job } = await graphqlRequest(query, { id });
+  return job;
+}
+
+export async function loadJobs() {
+  const query = `{
+    jobs {
+      id
+      title
+      company{ 
+        id
+        name
+      }
+    }
+  }`;
+
+  const { jobs } = await graphqlRequest(query);
+  return jobs;
+}
+
+export async function loadCompany(id) {
+  const query = `
+  query companyQuery($id: ID!) {
+    company(id: $id) {
+      id
+      name
+      description
+      jobs{
+        id
+        title
+      }
+    }
+  }
+  `;
+
+  const { company } = await graphqlRequest(query, { id });
+  return company;
 }
--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,11 +1,25 @@
 const db = require('./db');
 
 const Query = {
+  company: (parent, { id }) => db.companies.get(id),
+  job: (parent, args) => db.jobs.get(args.id),
   jobs: () => db.jobs.list(),
+};
+
+const Mutation = {
+  createJob: (root, { input }) => {
+    const id = db.jobs.create(input);
+    return db.jobs.get(id);
+  },
 };
 
 const Job = {
   company: (job) => db.companies.get(job.companyId),
 };
 
-module.exports = { Query, Job };
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
+
+module.exports = { Query, Mutation, Job, Company };
